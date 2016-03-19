af7b55d58afc9c813b55ffb2c178df526e138cea
Ivan Smirnov <ifsmirnov@yandex.ru>
2016-03-19 18:01:23 +0300

    init and try rebuilding

--- base.cpp
+++ base.cpp
@@ -85,9 +85,12 @@ int satSpeed[maxn], camSpeed[maxn], camRange[maxn];
 pt camPosition[maxn];
 int lastMove[maxn];
 
+pt initPosition[maxn];
+
 struct Collection {
     int value;
     vector<pt> locs;
+    vector<pt> init;
     vector<pii> times;
 
     void removeLoc(const pt& loc) {
@@ -104,6 +107,18 @@ struct Collection {
         }
         return false;
     }
+
+    int totalTime() const {
+        int t = 0;
+        for (auto kv: times) {
+            t += kv.se - kv.fi + 1;
+        }
+        return t;
+    }
+
+    void reset() {
+        locs = init;
+    }
 };
 
 int ncol;
@@ -114,7 +129,8 @@ void scan() {
 
     scanf("%d", &nsat);
     forn(i, nsat) {
-        satPosition[i] = readPoint();
+        initPosition[i] = readPoint();
+        satPosition[i] = initPosition[i];
         scanf("%d%d%d", &satSpeed[i], &camSpeed[i], &camRange[i]);
     }
 
@@ -134,27 +150,22 @@ void scan() {
                 Y = max(Y, p.y);
             }
         }
-        cerr << max(X-x, Y-y) << "\n";
+//         cerr << max(X-x, Y-y) << "\n";
         forn(j, r) {
             int b, e;
             scanf("%d%d", &b, &e);
             cols[i].times.eb(b, e);
         }
+        cols[i].init = cols[i].locs;
     }
 
-    sort(cols, cols + ncol, [](const Collection& lhs, const Collection& rhs) {
-        return lhs.locs.size() < rhs.locs.size();
-        return lhs.value > rhs.value;
-    });
-    ncol *= 0.35;
-
-    /*
     fprintf(stderr,"%d sattelites\n", nsat);
     forn(i, nsat) {
         fprintf(stderr,"speed: %d  cam: %d  range: %d  (max time to move: %d)\n",
             satSpeed[i], camSpeed[i], camRange[i], (camRange[i] + camSpeed[i] - 1) / camSpeed[i]);
     }
     fprintf(stderr,"\n");
+    /*
 
     fprintf(stderr,"%d collections\n", ncol);
     sort(cols, cols + ncol, [](const Collection& lhs, const Collection& rhs) {
@@ -211,6 +222,30 @@ void buildSpatialIndex() {
     cerr << "total points: " << total << endl;
     totalPoints = total;
 }
+
+void rebuildAll() {
+    forn(i, nsat) {
+        satPosition[i] = initPosition[i];
+        camPosition[i] = {0,0};
+    }
+
+    curTime = 0;
+
+    for (int x: availX) {
+        yset(x).clear();
+    }
+    availX.clear();
+
+    buildSpatialIndex();
+
+    result.clear();
+    score = 0;
+
+    forn(i, ncol) {
+        cols[i].reset();
+    }
+}
+
 
 vector<pair<pt, int>> closePoints(pt origin, int dist) {
     if (availX.empty()) return {};
@@ -276,13 +311,6 @@ void advanceSatellites() {
     }
 }
 
-void removeCollection(int colId) {
-    --ncol;
-    for (int i = colId; i < ncol; ++i) {
-        cols[i] = cols[i + 1];
-    }
-}
-
 inline int divideBy(int x, int y) {
     return (abs(x) + y - 1) / y;
 }
@@ -298,96 +326,93 @@ bool canTakePhoto(int satId, pt pos) {
 }
 
 double value(const pair<pt, int> &a) {
-    assert(!cols[a.second].locs.empty());
-    return cols[a.second].value;// / (double) sz(cols[a.second].locs);
+    return a.second;
+//     assert(!cols[a.second].locs.empty());
+//     return cols[a.second].value;// / (double) sz(cols[a.second].locs);
 }
 
 bool cmp(const pair<pt, int> &a, const pair<pt, int> &b) {
     return value(a) < value(b);
 }
 
+bool badComp[maxn];
+
 void solve() {
-    while (curTime <= T && ncol > 0) {
-        forn(satId, nsat) {
-            auto closePt = closePoints(satPosition[satId], camRange[satId]);
-            if (closePt.empty()) {
-                continue;
+    memset(badComp, 0, sizeof badComp);
+
+    while (true) {
+        while (curTime <= T && ncol > 0) {
+            forn(satId, nsat) {
+                auto closePt = closePoints(satPosition[satId], camRange[satId]);
+                if (closePt.empty()) {
+                    continue;
+                }
+
+                vector<pair<pt, int>> candidates;
+                for (auto pp: closePt) {
+                    int i = pp.second;
+                    pt pos = pp.first;
+                    if (find(cols[i].locs.begin(), cols[i].locs.end(), pos) == cols[i].locs.end())
+                        continue;
+                    if (!cols[i].available())
+                        continue;
+                    if (!canTakePhoto(satId, pos))
+                        continue;
+                    candidates.push_back(pp);
+                }
+
+                if (candidates.empty())
+                    continue;
+
+                pair<pt, int> where = *min_element(all(candidates), cmp);
+                int i = where.second;
+                pt pos = where.first;
+
+                writeOperation(satId, pos);
+                cols[i].removeLoc(pos);
+                lastMove[satId] = curTime;
+                camPosition[satId] = pos - satPosition[satId];
+                if (cols[i].locs.empty()) {
+                    cerr << "done collection " << i << " @" << curTime << ", $" << cols[i].value << "\n";
+                    score += cols[i].value;
+                }
             }
 
-            vector<pair<pt, int>> candidates;
-            for (auto pp: closePt) {
-                int i = pp.second;
-                pt pos = pp.first;
-                if (find(cols[i].locs.begin(), cols[i].locs.end(), pos) == cols[i].locs.end())
-                    continue;
-                if (!cols[i].available())
-                    continue;
-                if (!canTakePhoto(satId, pos))
-                    continue;
-                candidates.push_back(pp);
+            advanceSatellites();
+            if (curTime % 1000 == 0) {
+    //             cerr << "timestamp: " << curTime << ", elapsed: " << clock()/1000 << " ms" << endl;
             }
-
-            if (candidates.empty())
-                continue;
-
-            auto pp = *min_element(candidates.begin(), candidates.end(), cmp);
-            int i = pp.second;
-            pt pos = pp.first;
-
-            writeOperation(satId, pos);
-            cols[i].removeLoc(pos);
-            lastMove[satId] = curTime;
-            camPosition[satId] = pos - satPosition[satId];
-            if (cols[i].locs.empty()) {
-                cerr << "done collection " << i << " @" << curTime << ", $" << cols[i].value << "\n";
-                score += cols[i].value;
-            }
-
-            //for (int i = ncol - 1; i >= 0; --i) {
-                //if (!cols[i].available()) {
-                    //continue;
-                //}
-                //bool done = false;
-                //for (pt pos: cols[i].locs) {
-                    //if (canTakePhoto(satId, pos)) {
-                        //done = true;
-                        //writeOperation(satId, pos);
-                        //cols[i].removeLoc(pos);
-
-                        //lastMove[satId] = curTime;
-                        //camPosition[satId] = pos - satPosition[satId];
-
-                        //done = true;
-                        //break;
-                    //}
-                //}
-
-                //if (done) {
-                    //if (cols[i].locs.empty()) {
-                        //cerr << "done collection " << i << " @" << curTime << ", $" << cols[i].value << "\n";
-                        //score += cols[i].value;
-                        //removeCollection(i);
-                    //}
-                    //break;
-                //}
-            //}
         }
 
-        advanceSatellites();
-        if (curTime % 1000 == 0) {
-            cerr << "timestamp: " << curTime << ", elapsed: " << clock()/1000 << " ms" << endl;
+        int doneCols = 0;
+        int leftPoints = 0;
+        forn(i, ncol) {
+            doneCols += (cols[i].locs.empty());
+            leftPoints += cols[i].locs.size();
+
+            badComp[i] = !cols[i].locs.empty() && cols[i].locs.size() < cols[i].init.size();
         }
+
+        cerr << "Components done: " << doneCols << " of " << ncol << endl;
+        cerr << "Points killed: " << totalPoints - leftPoints << " of " << totalPoints << endl;
+        cerr << "Score: " << score << endl;
+
+        int old_nc = ncol;
+        for (int i = 0; i < ncol; ++i) {
+            if (badComp[i]) {
+                swap(cols[i], cols[ncol - 1]);
+                --ncol;
+                --i;
+            }
+        }
+        if (old_nc == ncol) {
+            break;
+        }
+        rebuildAll();
+        cerr << "rebuilt" << endl;
     }
 
     dumpOutput();
-    int doneCols = 0;
-    int leftPoints = 0;
-    forn(i, ncol) {
-        doneCols += (cols[i].locs.empty());
-        leftPoints += cols[i].locs.size();
-    }
-    cerr << "Components done: " << doneCols << " of " << ncol << endl;
-    cerr << "Points killed: " << totalPoints - leftPoints << " of " << totalPoints << endl;
 }
 
 int main() {
